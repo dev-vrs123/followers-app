6dbbb635daf36d26775926cbe1cceb878bcd40ca
kira-vrs123 <37355492+kira-vrs123@users.noreply.github.com>
2019-02-04 12:18:59 +0900

    読み込むべきファイルを全てindex.htmlに飛ばしてしまうっぽいので、index.htmlのみ外してみる。

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist'));
 // .... ここにAPI endpointを追加する処理を入れる。
 
 app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/deploy-demo/index.html');
+  res.status(200).sendFile(__dirname + '/dist/deploy-demo/');
 }); // /followersなどもindex.htmlに転送して処理させる。
 
 app.listen(process.env.PORT || 8080);
