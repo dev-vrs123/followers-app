fa79b03e00d80b7a106dc840f29a026baf9ee73c
kira-vrs123 <37355492+kira-vrs123@users.noreply.github.com>
2019-02-04 12:40:15 +0900

    こうする事で、ローカルでのテストは上手くいった。

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/deploy-demo/'));
 
 // .... ここにAPI endpointを追加する処理を入れる。
 
 app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/deploy-demo/' + __filename);
+  res.status(200).sendFile(__dirname + '/dist/deploy-demo/index.html');
 }); // /followersなどもindex.htmlに転送して処理させる。
 
 app.listen(process.env.PORT || 8080);
